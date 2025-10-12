11bac47d4a04461328527123128e20838c8096d1
Humakt83 <urlact@yahoo.com>
2025-10-13 01:04:56 +0300

    Tool to read card data from api

---
+++ tools/card-names-to-data-json/index.js
@@ -0,0 +1,48 @@
+import csv from 'csv-parser';
+import fs from 'fs'
+import axios from 'axios';
+
+const apiEndpoint = 'https://api.magicthegathering.io/v1/cards?name=';
+const results = [];
+
+function writeToJson(cards) {
+    fs.writeFile('cube.json', JSON.stringify(cards), 'utf8', () => console.log('Finished'));
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function readMtG() {
+    return await Promise.all(results.map(async (r, idx) => {
+        await sleep(200 * idx);
+        const response = await axios.get(apiEndpoint + r, { responseType: 'json', headers: {"User-Agent": 'card-names-to-data-json/0.1', Accept: 'application/json'}});
+        const first = response.data.cards[0];        
+        if (!first) {
+            console.log(r, response.data);
+            return {name: r};
+        }
+        return {
+            name: first.name, 
+            manaCost: first.manaCost,
+            cmc: first.cmc,
+            colors: first.colors,
+            colorIdentity: first.colorIdentity,
+            types: first.types,
+            subtypes: first.subtypes,
+            text: first.text,
+            power: first.power,
+            toughness: first.toughness,
+            imageUrl: first.imageUrl,
+        };
+    }));
+}
+
+fs.createReadStream('cube.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data.Name))
+  .on('end', () => {
+    readMtG().then(writeToJson);
+  });
